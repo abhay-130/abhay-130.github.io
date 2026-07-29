8aceeff5d38bcf587b32857336b98d9bec5fe52a
Abhay Kishor <abhaykishor130@gmail.com>
2026-07-29 15:01:21 +0530

    add projects to sanity

--- studio/schemaTypes/index.ts
+++ studio/schemaTypes/index.ts
@@ -1,7 +1,10 @@
 import { postType } from './post';
 import socialPost from './socialPost';
+// schemaTypes/index.ts (or schemas/index.js)
+import project from './project'
 
 export const schemaTypes = [
+  project,
   postType,
   socialPost,
 ];
